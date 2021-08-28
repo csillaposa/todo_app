96cd4ef9f496c2053038ea82fced40fabfb05606
Csilla Pósa <posa.csilla@gmail.com>
2021-08-28 14:47:29 +0200

    Added notes

--- public/browser.js
+++ public/browser.js
@@ -1,7 +1,9 @@
+//to set up what should happen if one of the edit buttons are clicked
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter new text");
         //post returns a promise
+        //a promise is very useful when we do not know how long an action is going to take
         axios.post('/update-item', {text: userInput}).then(function() {
             //comes later
         }).catch(function() {
