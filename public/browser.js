381e0d44e539999538a1709ba2f291822174e9f3
Csilla Pósa <posa.csilla@gmail.com>
2021-08-29 13:45:35 +0200

    If cancel is clicked, it is not set to a blank value

--- public/browser.js
+++ public/browser.js
@@ -4,10 +4,12 @@ document.addEventListener("click", function(e) {
         let userInput = prompt("Enter new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
         //post returns a promise
         //a promise is very useful when we do not know how long an action is going to take
-        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
-            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
-        }).catch(function() {
-            console.log("Please try again later")
-        });
+        if (userInput) {
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+            }).catch(function() {
+                console.log("Please try again later")
+            });
+        }
     }
 })
