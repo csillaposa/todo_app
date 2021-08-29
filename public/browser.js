919447c5f1e39c13ba1ae759f485ea609a9b4483
Csilla Pósa <posa.csilla@gmail.com>
2021-08-29 13:43:03 +0200

    Prepopulate the prompt with the editable text

--- public/browser.js
+++ public/browser.js
@@ -1,7 +1,7 @@
 //to set up what should happen if one of the edit buttons are clicked
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("edit-me")) {
-        let userInput = prompt("Enter new text");
+        let userInput = prompt("Enter new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
         //post returns a promise
         //a promise is very useful when we do not know how long an action is going to take
         axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
